1a5e9f59946d31b63dfc38f04e1f0a8e6391b190
MaheshMergu01 <maheshmergu14@gmail.com>
2026-07-18 19:20:51 +0530

    Function.StartGame

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ var board = [];
 var rows = 9;
 var columns = 9;
 var score = 0;
+
+
+window.onload = function() {
+    startGame();
+}
+
+function startGame() {
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++){
+            //Img tag <id "0-0">
+            let tile = document.createElement("img");
+            tile.id = r.toString() + '-' + c.toString();
+            tile.src = "./Images" + randomCandy() + ".png"
+        }
+    }
+}
