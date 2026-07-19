1d35f118b44bf26c2b57ec9e9e72f8631abdbb2c
MaheshMergu01 <maheshmergu14@gmail.com>
2026-07-19 19:22:49 +0530

    Error

--- script.js
+++ script.js
@@ -4,24 +4,27 @@ var rows = 9;
 var columns = 9;
 var score = 0;
 
-
 window.onload = function() {
-    startGame();
-}
+  startGame();
+};
 
 function randomCandy() {
-    return candies[Math.floor(math.random() * candies.length)]; //Candies 0-5.95
+  return candies[Math.floor(math.random() * candies.length)]; //Candies 0-5.99
 }
 
-
 function startGame() {
-    for (let r = 0; r < rows; r++) {
-        let row = [];
-        for (let c = 0; c < columns; c++){
-            //Img tag <id "0-0">
-            let tile = document.createElement("img");
-            tile.id = r.toString() + '-' + c.toString();
-            tile.src = "./Images" + randomCandy() + ".png"
-        }
+  for (let r = 0; r < rows; r++) {
+    let row = [];
+    for (let c = 0; c < columns; c++) {
+      //Img tag <id "0-0"> <src="./Images/..png">
+      let tile = document.createElement("img");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.src = "./images/" + randomCandy() + ".png";
+
+      document.getElementById("board").append(tile);
+      row.push(tile);
     }
+    board.push(row);
+  }
+  console.log(board);
 }
